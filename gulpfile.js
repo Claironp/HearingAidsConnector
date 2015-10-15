58bd84683a352f117cbab2df5aece9f5becb06ec
Samy Pessé <samypesse@gmail.com>
2015-10-15 20:11:03 +0200

    Base with ratchet

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
+var path = require('path');
 var less = require('gulp-less');
+var runSequence = require('run-sequence');
 var jetpack = require('fs-jetpack');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
+var rootDir = jetpack.cwd(__dirname);
+var ratchetDir = rootDir.cwd('node_modules/ratchet');
+var srcDir = rootDir.cwd('src');
+var wwwDir = rootDir.cwd('www');
+
+gulp.task('clean', function() {
+    wwwDir.dir('.', { empty: true });
+});
+
 gulp.task('build-js', function() {
-    return browserify('./src/index.js')
+    return browserify('./src/index.js', {
+            "transform": ["reactify"]
+        })
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('./www/js'));
@@ -13,11 +26,18 @@ gulp.task('build-js', function() {
 
 gulp.task('build-less', function () {
     return gulp.src('./src/stylesheets/index.less')
-    .pipe(less())
+    .pipe(less({
+         paths: [ path.join(__dirname, 'node_modules') ]
+    }))
     .pipe(gulp.dest('./www/css'));
 });
 
-gulp.task('build', ['build-less', 'build-js']);
+gulp.task('build', ['build-less', 'build-js'], function() {
+    srcDir.copy('index.html', wwwDir.path('index.html'));
+    ratchetDir.copy('dist/fonts', wwwDir.path('fonts'));
+});
 
 
-gulp.task('default', ['build']);
+gulp.task('default', function(cb) {
+    runSequence('clean', 'build', cb);
+});
--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-var react = require('react');
-var reactDOM = require('react-dom');
+var React = require('react');
+
+var NavBar = require('react-ratchet').NavBar;
+var NavButton = require('react-ratchet').NavButton;
+var Title = require('react-ratchet').Title;
+
+var Application = React.createClass({
+    render: function() {
+        return <NavBar>
+            <NavButton left>Left</NavButton>
+            <NavButton right>Right</NavButton>
+            <Title>Title</Title>
+        </NavBar>;
+    }
+});
+
+React.render(<Application />, document.getElementById('app'));
