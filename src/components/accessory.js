ac87638a0919575e42bfdaae151691866da8d24e
Samy Pessé <samypesse@gmail.com>
2015-10-20 19:20:23 +0200

    Improve detail page for accessory

--- src/components/accessory.js
+++ src/components/accessory.js
@@ -1,7 +1,12 @@
+var _ = require('lodash');
 var React = require('react');
 var Tab = require('./tab');
+var Link = require('react-router').Link;
 
 var accessories = require('../lib/accessories');
+var brands = require('../lib/brands');
+var medias = require('../lib/medias');
+var MediasList = require('./lists/medias');
 
 var Accessory = React.createClass({
     propTypes: {
@@ -12,17 +17,33 @@ var Accessory = React.createClass({
         var accessory = accessories.get(this.props.params.id);
         if (!accessory) return <Tab></Tab>;
 
+        var accmedias = _.map(accessory.medias, function(mediaId) {
+            return medias.get(mediaId);
+        });
+
+        var brand = brands.get(accessory.brand);
+
         return <Tab title="Accessoire" back={true} backLabel="Back">
             <div id="tab-accessory" className="content">
                 <div className="accessory-intro">
                     <div className="content-padded">
                         <img src={accessory.image} />
                         <h3>{accessory.title}</h3>
+                        <p className="content-padded">{accessory.description}</p>
                     </div>
                 </div>
-                <div className="accessory-block">
-                    <p className="content-padded">{accessory.description}</p>
-                </div>
+                <p className="content-padded">Cet accessoire marche avec les medias:</p>
+                <MediasList medias={accmedias} />
+
+                <ul className="table-view">
+                    <li className="table-view-cell media">
+                        <Link to={'/accessories/brand/'+brand.id}  className="navigate-right">
+                            <div className="media-body">
+                                Voir tous les accessoires pour {brand.title}
+                            </div>
+                        </Link>
+                    </li>
+                </ul>
             </div>
             <div className="bar bar-standard bar-footer-secondary">
                 <button className="btn btn-block">Acheter</button>
