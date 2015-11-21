e48106308f9e61c06b7468070e9a80e26ea2b204
Claironp <pelissier.claire@gmail.com>
2015-11-21 18:14:11 +0100

    personnalisation "about"

--- src/components/about.js
+++ src/components/about.js
@@ -6,8 +6,9 @@ var AboutTab = React.createClass({
         return <Tab title='A propos'>
             <div id="tab-about" className="content">
                 <div className="content-padded">
-                    <h5>Dans le cadre de lélaboration de mon mémoire de troisième année en audioprothèse, j'ai choisi de développer cette application visant à faciliter la recherche d'accessoires pour aides auditives : conditions dutilisation, documentation, modes demploi, compatibilité. Toutes ces informations sont désormais accessibles facilement, et surtout rapidement.  <br/> <br/> <br/> <br/>Claire Pélissier <br/>Troisième année daudioprothèse <br/>Institut des Sciences et Techniques de la Réadaptation <br/>UNIVERSITE CLAUDE BERNARD LYON 1 </h5>
+                    <h5>Dans le cadre de l'élaboration de mon mémoire de troisième année en audioprothèse, j'ai choisi de développer cette application visant à faciliter la recherche d'accessoires pour aides auditives : conditions dutilisation, documentation, modes d'emploi, compatibilité. Toutes ces informations sont désormais accessibles facilement, et surtout rapidement <br/> <br/> <br/> <br/></h5>
                 	<img src="images/claire.png" className="photo-claire"/>
+                	<h5 className="text-center">Claire Pélissier <br/>pelissier.claire@gmail.com <br/>Troisième année daudioprothèse <br/>Institut des Sciences et Techniques de la Réadaptation <br/>UNIVERSITE CLAUDE BERNARD LYON 1</h5>
                 </div>
             </div>
         </Tab>;
