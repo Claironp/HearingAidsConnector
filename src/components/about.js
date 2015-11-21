3595c83bd2b40555bbfe9ead4477981cd83c6fc3
Claironp <pelissier.claire@gmail.com>
2015-11-21 17:55:53 +0100

    claire

--- src/components/about.js
+++ src/components/about.js
@@ -7,6 +7,7 @@ var AboutTab = React.createClass({
             <div id="tab-about" className="content">
                 <div className="content-padded">
                     <h5>Dans le cadre de lélaboration de mon mémoire de troisième année en audioprothèse, j'ai choisi de développer cette application visant à faciliter la recherche d'accessoires pour aides auditives : conditions dutilisation, documentation, modes demploi, compatibilité. Toutes ces informations sont désormais accessibles facilement, et surtout rapidement.  <br/> <br/> <br/> <br/>Claire Pélissier <br/>Troisième année daudioprothèse <br/>Institut des Sciences et Techniques de la Réadaptation <br/>UNIVERSITE CLAUDE BERNARD LYON 1 </h5>
+                	<img src="images/claire.png"/>
                 </div>
             </div>
         </Tab>;
