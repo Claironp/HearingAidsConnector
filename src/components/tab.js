53f2aeb21164d2ce8a4aef8cad55cf663a6a618d
Samy Pessé <samypesse@gmail.com>
2015-11-29 19:14:22 +0100

    Fix keyboard for search

--- src/components/tab.js
+++ src/components/tab.js
@@ -3,9 +3,15 @@ var React = require('react');
 var FooterNav = require('./footer');
 var HeaderNav = require('./header');
 
+
+
 var Tab = React.createClass({
     propTypes: {
         title: React.PropTypes.string
+    },
+
+    componentDidMount: function() {
+        window.scrollTo(0, 0);
     },
 
     render: function() {
--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ var SearchTab = require('./components/search');
 var ErrorTab = require('./components/error');
 
 
+function isTextInput(node) {
+    return ['INPUT', 'TEXTAREA'].indexOf(node.nodeName) !== -1;
+}
+
+document.addEventListener('touchstart', function (e) {
+    if (!isTextInput(e.target) && isTextInput(document.activeElement)) {
+        document.activeElement.blur();
+    }
+}, false);
+
 ReactDOM.render(
     <router.Router>
         <router.Route path="/" component={BrandsTab} />
